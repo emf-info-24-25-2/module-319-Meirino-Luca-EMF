5638ec3fcc89b10a8853ae7f5c1f8c5dfcffa971
Meirino-Luca-EMF <Luca.meirino@studentfr.ch>
2024-09-10 20:22:48 +0200

    Mon Devoir01

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,17 @@
+package devoirs.devoir01;
+
+public class devoir01 {
+    public static void main(String[] args)
+    {
+    int monAge = 20;
+        
+    if ( monAge >= 18 ) 
+    {
+        System.out.println("est majeurs");
+    }
+    else if ( monAge < 18 ) 
+    {
+        System.out.println("est pas majeurs");
+    }
+    }
+}
