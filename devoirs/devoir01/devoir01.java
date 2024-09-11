4a96e7f669969b7ec1eef11fcdec1d269109e175
Fanny Riedo <141828170+riedo-emf@users.noreply.github.com>
2024-09-11 23:55:33 +0200

    correction devoir1

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -4,6 +4,11 @@ public class devoir01 {
     public static void main(String[] args)
     {
     int monAge = 20;
+
+        
+        //Relisez bien la donnée, il faut déclarer une variable estMajeur 
+        //qui permet de stocker si oui ou non la personne est majeure.
+        //changez ceci et corrigez la suite de l'exercice
         
     if ( monAge >= 18 ) 
     {
