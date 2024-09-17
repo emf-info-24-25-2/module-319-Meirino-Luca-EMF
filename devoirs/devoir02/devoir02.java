3b170d9fe241fd8c261a49b23cb73aa0789326a8
Meirino-Luca-EMF <Luca.meirino@studentfr.ch>
2024-09-17 21:28:23 +0200

    mon commit

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -1,6 +1,6 @@
 package devoirs.devoir02;
 
-public class devoir02 { 
+public class devoir02 {
 
     public static final double CAPA_RES_A = 3;
     public static final double CAPA_RES_B = 5;
@@ -10,19 +10,17 @@ public class devoir02 {
         int resA = 0;
         int resB = 0;
 
-        int remp = ( int ) ( Math.random() * ( 8 - 3 + 1 ) ) + 3;
+        int remp = (int) (Math.random() * (8 - 3 + 1)) + 3;
 
         System.out.println("Il y a " + remp + " litres à remplir.");
 
-        while ( resA < CAPA_RES_A ) 
-        {
+        while (resA < CAPA_RES_A) {
             remp--;
             resA++;
             System.out.println("Remplissage du réservoir A...");
             System.out.println("Le réservoir A : " + resA + ", le réservoir B : " + resB);
         }
-        while ( remp > 0) 
-        {
+        while (remp > 0) {
             remp--;
             resB++;
             System.out.println("Remplissage du réservoir B...");
