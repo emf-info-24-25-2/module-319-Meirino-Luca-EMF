4264e4cd49e6f8f4bd4f3c7e63010f1a18cd45f7
Meirino.Luca.emf <Luca.Meirino@studentfr.ch>
2024-09-19 11:26:58 +0200

    ajout d'un commentaire par F.Riedo

--- exercices/exercice08/Exercice8/src/ExerciceBoucle1.java
+++ exercices/exercice08/Exercice8/src/ExerciceBoucle1.java
@@ -2,6 +2,7 @@ public class ExerciceBoucle1 {
 
     public static void main(String[] args) {
 
+        //boucle for
         for (int i = 1; i < 6; i++) {
             System.out.println("i:" + i);
         }
