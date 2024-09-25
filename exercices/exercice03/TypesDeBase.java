d60ca4a93e658bf1e7f89cb0d977b76efc760199
Meirino-Luca-EMF <Luca.meirino@studentfr.ch>
2024-09-25 19:26:50 +0200

    Mon commit

--- exercices/exercice03/TypesDeBase.java
+++ exercices/exercice03/TypesDeBase.java
@@ -1,10 +1,6 @@
-public class TypesDeBase {
+public class TypesDeBase{
     public static void main(String[] args) {
-        int age = 15;
-        String nom = "Luca ";
-
-        System.out.println("Je m'appelle "+ nom +"et j'ai " +age+ "ans.");
-        age += 10;
-        System.out.println("Et dans 10 ans j'aurai "+ age);
+        int age =0;
+        int nombre;   
     }
-}
+}   
