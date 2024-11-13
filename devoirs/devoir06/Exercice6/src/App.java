ca2aca3d7a6a9632516ace2491b7a72ba7d621a4
Fanny Riedo <141828170+riedo-emf@users.noreply.github.com>
2024-11-13 18:21:34 +0100

    Devoir incomplet

--- devoirs/devoir06/Exercice6/src/App.java
+++ devoirs/devoir06/Exercice6/src/App.java
@@ -9,6 +9,8 @@ public class App {
     public static int[] genereTableau(){
         int random = ( int ) ( Math.random() * ( VALEUR_MAX - VALEUR_MIN + 1 ) ) + VALEUR_MIN; 
         int[] tableauGenere = new int[TAILLE_TABLEAU];
+
+        //RIF: attention si vous faites comme ça vous calculez une seule valeur aléatoire et vous mettez la même dans toutes les cellules du tableau
         for (int i = 0; i < tableauGenere.length; i++) {
         tableauGenere[i] = random;
         }
